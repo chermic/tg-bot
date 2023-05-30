42d842c3200dd3edc5ea8b0fcda2db33a8b684f7
Mikhail Chernyshenko <chernyshenkomd@intech.rshb.ru>
2023-05-30 23:01:11 +0300

    chore(*): replace fastify with express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import express from "express";
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import { config } from "dotenv";
@@ -16,6 +17,8 @@ config();
 //   bot.launch();
 // };
 
+const app = express();
+
 const port =
   typeof process.env.PORT === "string" && process.env.PORT?.length > 0
     ? Number.parseInt(process.env.PORT, 10)
@@ -28,41 +31,38 @@ const telegramUrl = new URL(
   `/bot${process.env.BOT_TOKEN}/sendMessage`,
   baseTgUrl
 );
-console.log('🚀 ~ file: index.js:31 ~ telegramUrl:', telegramUrl);
+console.log("🚀 ~ file: index.js:31 ~ telegramUrl:", telegramUrl);
 const jokeUrl = "https://v2.jokeapi.dev/joke/Programming?type=single";
 
-fastify.get("/", (request, response) => {
-  response.send({ ok: true });
+app.get("/", (request, response) => {
+  response.json({ ok: true });
 });
 
-fastify.post("/new-message", async (request, response) => {
+app.post("/new-message", async (request, response) => {
   try {
-    const message = (request.body)?.message;
+    const message = (request.body as { message: any })?.message;
     const text = message?.text?.toLowerCase()?.trim();
     const chatId = message?.chat?.id;
 
-    const tgResponse = await fetch(telegramUrl, {
-      method: "POST",
-      body: JSON.stringify({ chat_id: chatId, text }),
-    });
-    console.log(
-      "🚀 ~ file: index.ts:38 ~ fastify.post ~ tgResponse:",
-      tgResponse
-    );
+    // const tgResponse = await fetch(telegramUrl, {
+    //   method: "POST",
+    //   body: JSON.stringify({ chat_id: chatId, text }),
+    // });
+    // console.log(
+    //   "🚀 ~ file: index.ts:38 ~ fastify.post ~ tgResponse:",
+    //   tgResponse
+    // );
+    const jokeResponse = await fetch(jokeUrl);
+    jokeResponse.json().then(console.log)
 
-    response.type("application/json").code(200);
-    return { ok: true };
+    response.status(200).json({ ok: true });
   } catch (error) {
     console.log("🚀 ~ file: index.ts:43 ~ fastify.post ~ error:", error);
-    response.type("application/json").code(400);
-    return { ok: false, error: error instanceof Error ? error.message : null };
+    response.status(400).json({ ok: false, error });
+    // return { ok: false, error: error instanceof Error ? error.message : null };
   }
 });
 
-fastify.listen({ port }, (error, address) => {
-  if (error) {
-    throw error;
-  }
-
+app.listen({ port }, () => {
   console.log(`Server is listening on ${port}`);
 });
