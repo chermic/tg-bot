07d0d5667d3c5f318478767be18d1c7592ded735
Mikhail Chernyshenko <chernyshenkomd@intech.rshb.ru>
2023-05-31 00:05:53 +0300

    chore(*): comment obsolete code

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,64 @@
-import express from "express";
-import { Telegraf } from "telegraf";
-import { message } from "telegraf/filters";
-import { config } from "dotenv";
-config();
+// import express from "express";
+// import { Telegraf } from "telegraf";
+// import { message } from "telegraf/filters";
+// import { config } from "dotenv";
+// config();
 
-// const bot = new Telegraf(process.env.BOT_TOKEN ?? "");
+// // const bot = new Telegraf(process.env.BOT_TOKEN ?? "");
 
-// bot.on("message", (ctx) => {
-//   const { id } = ctx.chat;
+// // bot.on("message", (ctx) => {
+// //   const { id } = ctx.chat;
 
-//   ctx.reply("Reply");
+// //   ctx.reply("Reply");
+// // });
+
+// // const launchBot = () => {
+// //   bot.launch();
+// // };
+
+// const app = express();
+
+// const port =
+//   typeof process.env.PORT === "string" && process.env.PORT?.length > 0
+//     ? Number.parseInt(process.env.PORT, 10)
+//     : 3000;
+
+// const baseTgUrl = "https://api.telegram.com";
+// const telegramUrl = new URL(
+//   `/bot${process.env.BOT_TOKEN}/sendMessage`,
+//   baseTgUrl
+// );
+// console.log("🚀 ~ file: index.js:31 ~ telegramUrl:", telegramUrl);
+// const jokeUrl = "https://v2.jokeapi.dev/joke/Programming?type=single";
+
+// app.get("/", (request, response) => {
+//   response.json({ ok: true });
 // });
 
-// const launchBot = () => {
-//   bot.launch();
-// };
+// app.post("/new-message", async (request, response) => {
+//   try {
+//     const message = (request.body as { message: any })?.message;
+//     const text = message?.text?.toLowerCase()?.trim();
+//     const chatId = message?.chat?.id;
 
-const app = express();
+//     // const tgResponse = await fetch(telegramUrl, {
+//     //   method: "POST",
+//     //   body: JSON.stringify({ chat_id: chatId, text }),
+//     // });
+//     // console.log(
+//     //   "🚀 ~ file: index.ts:38 ~ fastify.post ~ tgResponse:",
+//     //   tgResponse
+//     // );
+//     const jokeResponse = await fetch(jokeUrl);
+//     jokeResponse.json().then(console.log);
 
-const port =
-  typeof process.env.PORT === "string" && process.env.PORT?.length > 0
-    ? Number.parseInt(process.env.PORT, 10)
-    : 3000;
+//     response.status(200).json({ ok: true });
+//   } catch (error) {
+//     console.log("🚀 ~ file: index.ts:43 ~ fastify.post ~ error:", error);
+//     response.status(400).json({ ok: false, error });
+//   }
+// });
 
-const baseTgUrl = "https://api.telegram.com";
-const telegramUrl = new URL(
-  `/bot${process.env.BOT_TOKEN}/sendMessage`,
-  baseTgUrl
-);
-console.log("🚀 ~ file: index.js:31 ~ telegramUrl:", telegramUrl);
-const jokeUrl = "https://v2.jokeapi.dev/joke/Programming?type=single";
-
-app.get("/", (request, response) => {
-  response.json({ ok: true });
-});
-
-app.post("/new-message", async (request, response) => {
-  try {
-    const message = (request.body as { message: any })?.message;
-    const text = message?.text?.toLowerCase()?.trim();
-    const chatId = message?.chat?.id;
-
-    // const tgResponse = await fetch(telegramUrl, {
-    //   method: "POST",
-    //   body: JSON.stringify({ chat_id: chatId, text }),
-    // });
-    // console.log(
-    //   "🚀 ~ file: index.ts:38 ~ fastify.post ~ tgResponse:",
-    //   tgResponse
-    // );
-    const jokeResponse = await fetch(jokeUrl);
-    jokeResponse.json().then(console.log);
-
-    response.status(200).json({ ok: true });
-  } catch (error) {
-    console.log("🚀 ~ file: index.ts:43 ~ fastify.post ~ error:", error);
-    response.status(400).json({ ok: false, error });
-  }
-});
-
-app.listen({ port }, () => {
-  console.log(`Server is listening on ${port}`);
-});
+// app.listen({ port }, () => {
+//   console.log(`Server is listening on ${port}`);
+// });
